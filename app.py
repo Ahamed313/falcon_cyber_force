aaaee164e308cb36aa3fd9104ff6dacb8d5908c7
Ahamed313 <sabujkhagatua@gmail.com>
2025-12-18 21:56:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,15 +3,8 @@ from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
 app.secret_key = "falcon_secret_key"
 
-# Dummy admin credentials (later DB use করা যাবে)
 ADMIN_USERNAME = "admin"
 ADMIN_PASSWORD = "admin123"
-
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -25,19 +18,13 @@ def login():
 
     return render_template("login.html")
 
-
 @app.route("/admin")
 def admin_dashboard():
     if not session.get("admin"):
         return redirect(url_for("login"))
     return render_template("dashboard.html")
 
-
 @app.route("/logout")
 def logout():
     session.pop("admin", None)
     return redirect(url_for("login"))
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
